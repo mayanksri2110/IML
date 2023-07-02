1806e960bf29a45adac8c7d101ef452c94fe2b70
mayanksri2110 <134602536+mayanksri2110@users.noreply.github.com>
2023-07-02 22:58:26 +0530

    Update 05b-mle-cameraman.py

--- 05b-mle-cameraman.py
+++ 05b-mle-cameraman.py
@@ -1,10 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy.stats as stats
+from scipy import stats
 import cv2
 
 # Read the image as an array
-image = cv2.imread('/content/kids.tif')
+image = cv2.imread('/content/space.tif')
 
 # Convert the image to grayscale and normalize the values between 0 and 1
 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) / 255
@@ -16,11 +16,11 @@ T = 100
 repeated_image = np.repeat(gray_image[:, :, np.newaxis], T, axis=2)
 
 # Generate random values following a Poisson distribution based on the repeated image
-x = stats.poisson.rvs(repeated_image)
+random_poisson = np.random.poisson(repeated_image)
 
 # Create a binary mask based on a threshold value
 threshold = 1
-binary_mask = (x >= threshold).astype(float)
+binary_mask = (random_poisson >= threshold).astype(float)
 
 # Estimate the parameter using the binary mask
 estimated_parameter = -np.log(1 - np.mean(binary_mask, axis=2))
